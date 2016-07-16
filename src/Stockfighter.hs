c8aa7a75eda62955844414c20fa386337c3f2f26
Adam Hinz <adam@zensight.co>
2016-07-16 12:36:04 -0400

    Add timeout
    Surprisingly hard...

--- src/Stockfighter.hs
+++ src/Stockfighter.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+-- https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading-levels-automagically/143
+
 module Stockfighter where
 
 import GHC.Generics
@@ -15,7 +17,10 @@ import qualified Data.Text.Encoding as Enc
 
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
-import           Network.HTTP.Simple
+
+import Network.HTTP.Simple
+import qualified Network.HTTP.Client.TLS as H
+import qualified Network.HTTP.Client as H
 
 data StockfighterApiKey  = StockfighterApiKey T.Text
 data InstanceId = InstanceId Integer
@@ -43,28 +48,63 @@ gmBaseUrl = "/gm"
 iToText :: Integer -> T.Text
 iToText = T.pack . show
 
-restartLevelUrl (InstanceId i) = T.concat [gmBaseUrl, i', "/restart"]
-  where i' = iToText i
-
 startLevelUrl :: T.Text -> T.Text
 startLevelUrl level = T.concat [gmBaseUrl, "/levels/", level]
 
-startLevelReq :: StockfighterApiKey -> T.Text -> Request
-startLevelReq (StockfighterApiKey k) t =
-  setRequestPath path
-  $ setRequestMethod "POST"
-  $ setRequestHost gmHost
+baseRequest :: StockfighterApiKey -> S8.ByteString -> T.Text -> Request
+baseRequest (StockfighterApiKey k) method path =
+  setRequestPath path'
+  $ setRequestMethod method
   $ setRequestSecure True
   $ setRequestPort 443
   $ setRequestHeader "Cookie" [cookie]
   $ defaultRequest
   where
     cookie = Enc.encodeUtf8 $ T.concat ["api_key=", k]
-    path = Enc.encodeUtf8 . startLevelUrl $ t
+    path' = Enc.encodeUtf8 path
+
+gmRequest :: StockfighterApiKey -> S8.ByteString -> T.Text -> Request
+gmRequest k m p =
+  setRequestHost gmHost $ baseRequest k m p
+
+startLevelRequest :: StockfighterApiKey -> T.Text -> Request
+startLevelRequest k t =
+  gmRequest k "POST" path
+  where
+    path = startLevelUrl t
+
+restartLevelUrl :: Level -> T.Text
+restartLevelUrl l = T.concat [gmBaseUrl
+                             , "/instances/"
+                             , instanceId
+                             , "/restart"]
+  where instanceId = iToText $ levelInstanceId l
+
+restartLevelRequest :: StockfighterApiKey -> Level -> Request
+restartLevelRequest k l =
+  gmRequest k "POST" path
+  where
+    path = restartLevelUrl l
+
+setDefaultTimeout :: H.Request -> H.Request
+setDefaultTimeout r =
+  r { H.responseTimeout = H.responseTimeoutMicro 120000000 }
+
+restartLevel :: StockfighterApiKey -> Level -> IO (Either JSONException Value)
+restartLevel k l =
+  let request = restartLevelRequest k l in
+  do
+    manager <- H.newManager H.tlsManagerSettings
+    response <- httpJSONEither (setRequestManager manager
+                                $ setDefaultTimeout request)
+    return $ getResponseBody response
+
+convert :: Either a (IO b) -> IO (Either a b)
+convert = either (return . Left) (fmap Right)
 
 startLevel :: StockfighterApiKey -> T.Text -> IO (Either JSONException Level)
 startLevel k t =
-  let request = startLevelReq k t in
+  let request = startLevelRequest k t in
   do
     response <- httpJSONEither request
     return $ toLevel <$> getResponseBody response
@@ -73,15 +113,3 @@ startLevel k t =
                       , tradingAccount = account a
                       , ticker = head $ tickers a
                       , venue = head $ venues a }
-
-
-main' :: StockfighterApiKey -> IO (StartLevelResponse)
-main' k =
-  let request = startLevelReq k "first_steps" in
-    do
-      response <- httpJSON request
-
-      putStrLn $ "The status code was: " ++
-        show (getResponseStatusCode response)
-      print $ getResponseHeader "Content-Type" response
-      return $ getResponseBody response
